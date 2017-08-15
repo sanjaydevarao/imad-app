1c1fb20a71c0c6cdbdfa88f046fa9b02b71c99bd
SANJAY DEVA RAO <sanjaydevarao1@gmail.com>
2017-08-15 21:11:46 +0530

    Js change

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
     app.get('/article1',function(req,res){
-        res.send("article1 is under service");
+          res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
     });
-    
+
      app.get('/article2',function(req,res){
         res.send("article2 is under service");
     });
-    
+
      app.get('/article3',function(req,res){
         res.send("article3 is under service");
     });
