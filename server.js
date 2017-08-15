c7d1881c5a7cc0c3a2ddc0d5141adfa750b63ac2
Sanjay Deva Rao <sanjaydevarao1@gmail.com>
2017-08-15 21:52:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/article1_img/fire1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/article1_img', 'fire1.jpg'));
+app.get('/ui/fire1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fire1.jpg'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
